2cde94ccb3ee77a212a0634962f2eee96e922be9
Oarabile Moima <omoima@c4r8s3.wethinkcode.co.za>
2020-07-09 12:17:05 +0200

    Added ft_putchar to ex00 to ex03

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,3 +1,7 @@
+void ft_putchar(char c){
+	write(1, &c, 1);
+}
+
 void ft_print_reverse_alphabet(void){
 	char c;
 	c = 'z';
--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -1,3 +1,7 @@
+void ft_putchar(char c){
+	write(1, &c, 1);
+}
+
 void ft_print_numbers(void){
 	int num;
 	int show;
