d5f12349c1aac40f7d0753c73d23dde38d1d6c1e
Oarabile Moima <omoima@c4r8s3.wethinkcode.co.za>
2020-07-09 13:40:11 +0200

    Correct way to add ft_puchar

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,6 +1,4 @@
-void ft_putchar(char c){
-	write(1, &c, 1);
-}
+void ft_putchar(char c);
 
 void ft_print_alphabet(void){
 	char c;
--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,6 +1,4 @@
-void ft_putchar(char c){
-	write(1, &c, 1);
-}
+void ft_putchar(char c);
 
 void ft_is_negative(int n){
 	if (n >= 0){
